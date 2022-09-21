da2ddefbd17ebad9846de97bb0055842be5ccdec
Timur_Akchurin <timur.akchl1@outlook.com>
2022-09-21 19:44:16 +0300

    end

--- test.js
+++ test.js
@@ -1,58 +1,91 @@
 const html = document.documentElement;
+const main = $("#main");
 const canvas = document.getElementById("hero-lightpass");
 const context = canvas.getContext("2d");
 
 const frameCount = 395;
-const currentFrame = index => (
-  `img/kitchen_c_${index.toString().padStart(3, '0')}.jpg`
-)
+let currentFrame = (index) => `img/kitchen_c_${index.toString().padStart(3, "0")}.jpg`;
 
 const preloadImages = () => {
-  for (let i = 1; i < frameCount; i++) {
-    const img = new Image();
-    img.src = currentFrame(i);
-  }
+    for (let i = 1; i < frameCount; i++) {
+        const img = new Image();
+        img.src = currentFrame(i);
+    }
 };
 
-const img = new Image()
+const img = new Image();
 img.src = currentFrame(1);
 canvas.width = 1900;
 canvas.height = 1000;
 img.onload = function () {
-  context.drawImage(img, 0, 0);
+    context.drawImage(img, 0, 0);
+};
+
+const updateImage = (index) => {
+    img.src = currentFrame(index);
+    context.drawImage(img, 0, 0);
+};
+
+// скорость скролла
+let scrollSpeed = 350;
+// скролл до мейна после анимации
+function scrollAfterAnim(block, speed, e) {
+    if (!block.hasClass("scrolled")) {
+        $("html,body").stop().animate({ scrollTop: block.offset().top }, speed);
+        e.preventDefault();
+        setTimeout(() => {
+            block.addClass("scrolled");
+        }, speed);
+    }
 }
 
-const updateImage = index => {
-  img.src = currentFrame(index);
-  context.drawImage(img, 0, 0);
+let scrollBefore = 0;
+// функция отслеживающая направление скролла
+function scrollDirection(e, frameIndex) {
+    let scrolled = window.scrollY;
+    // вверх
+    if (scrollBefore > scrolled) {
+        scrollBefore = scrolled;
+        if ($("#main").hasClass("scrolled") && html.scrollTop < $("#main").offset().top) {
+            $(".anim").removeClass("hide");
+            $("#main").removeClass("scrolled");
+        }
+        // вниз
+    } else {
+        scrollBefore = scrolled;
+        if (frameIndex == 394) {
+            // скролл к мейну после завершения анимации
+            scrollAfterAnim(main, scrollSpeed, e);
+            $(".anim").addClass("hide");
+        }
+    }
 }
+const blue = document.querySelector(".blue");
+window.addEventListener("scroll", (e) => {
+    let scrollTop = html.scrollTop - document.querySelector(".anim").scrollTop;
+    console.log("11", html.scrollTop - document.querySelector(".anim").scrollTop);
+    scrollTop < 0 ? (scrollTop = 0) : (scrollTop = scrollTop);
+    // console.log(scrollTop);
+    console.log(document.querySelector(".anim").offsetHeight);
+    const maxScrollTop = document.querySelector(".anim").offsetHeight / 2;
+    const scrollFraction = scrollTop / maxScrollTop;
 
-window.addEventListener('scroll', () => {
+    let frameIndex = Math.min(frameCount - 1, Math.ceil(scrollFraction * frameCount));
+    isNaN(frameIndex) ? (frameIndex = 1) : (frameIndex = frameIndex);
+    // console.log(frameIndex);
+    // показать/скрыть всплывающий текст
+    if (frameIndex > 40) {
+        $("#monitor-gui").removeClass("active");
+        $("#monitor-gui").addClass("noactive");
+    } else {
+        $("#monitor-gui").removeClass("noactive");
+        $("#monitor-gui").addClass("active");
+    }
 
-  // console.log(window.scrollY); // показываем пиксели
+    // отслеживание направления скролла + показать\скрыть анимацию
+    scrollDirection(e, frameIndex);
 
-  const scrollTop = html.scrollTop;
-  const maxScrollTop = html.scrollHeight - window.innerHeight;
-  const scrollFraction = scrollTop / maxScrollTop;
-  const frameIndex = Math.min(
-    frameCount - 1,
-    Math.ceil(scrollFraction * frameCount)
-  );
-  console.log(frameIndex);
-  if (frameIndex > 40) {
-    $('#monitor-gui').removeClass("active");
-    $('#monitor-gui').addClass("noactive");
-  }
-  else {
-    $('#monitor-gui').removeClass("noactive");
-    $('#monitor-gui').addClass("active");
-  }
-
-  if (frameIndex > 394) {
-    // $("#hero-lightpass").css('display', 'none');
-  }
-
-  requestAnimationFrame(() => updateImage(frameIndex + 1))
+    requestAnimationFrame(() => updateImage(frameIndex + 1));
 });
 
-preloadImages()
+preloadImages();
